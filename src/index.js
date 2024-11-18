b3f07f5ae970c003bb2d473b01e3f8d7566808d7
Dhirajsviqnet <155939260+Dhirajsviqnet@users.noreply.github.com>
2024-11-18 18:01:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173', // Allow requests from your frontend URL
+    origin: '*', // Allow requests from your frontend URL
     methods: ['GET', 'POST'],       // Allow GET and POST methods
     allowedHeaders: ['Content-Type'], // Optional, if you want to specify headers
     credentials: true,               // If you are using cookies, you can enable this
